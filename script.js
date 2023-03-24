227f94c162738e5938cc9db49de28f4cc6e58548
Jose Leyva <jrleyva41096@gmail.com>
2023-03-24 17:20:33 -0400

    implemented functions to populate cards with data

--- script.js
+++ script.js
@@ -3,56 +3,49 @@ var cityName = document.getElementById('cityInput');
 const apiKey = '3b45717e6a5c57fe595a9f2361b3126b'
 
 function getCurrentForecast(cityName) {
-    var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=metric`
-// console.log(res);
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data)
-            //Loop over the data to generate a list
-            
+  var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=metric`
+  // console.log(res);
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
 
-        })
+      const firstDay = data.list.splice(0,1)[0]
+      const firstDayDiv = buildDayDiv(firstDay)
+      document.querySelector('#currentForcast').appendChild(firstDayDiv)
+
+      //Loop over the data to generate a list
+        for (let day of data.list.slice(0,5)) {
+          const div = buildDayDiv(day)
+          document.querySelector('#fiveDayForecast').appendChild(div)
+        }
+
+    })
 }
-document.getElementById("cityInput").addEventListener("keyup", function(event) {
-  if (event.key === "Enter") {
-    getCurrentForecast(event.target.value);
-  }
-});
 
+function getWeather(event) {
+  event.preventDefault(); // Prevent the form from submitting and reloading the page
+  const city = document.getElementById("cityInput").value;
+  getCurrentForecast(city); // Call getCurrentForecast with the city input value
+}
 
+function buildDayDiv(day) {
+  const dayDiv = document.createElement("div");
+  dayDiv.classList.add ('castCard')
 
-// function getWeather() {
-//   const apiKey = '3b45717e6a5c57fe595a9f2361b3126b';
-//   const city = document.getElementById('cityInput').value;
+  const temp = document.createElement("p");
+  temp.textContent = `Temperature: ${day.main.temp} °F`;
+  dayDiv.appendChild(temp);
 
-//   fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`)
-//     .then(response => response.json())
-//     .then(data => {
-//       console.log(data);
+  const windSpeed = document.createElement("p");
+  windSpeed.textContent = `Wind Speed: ${day.wind.speed} mph`;
+  dayDiv.appendChild(windSpeed);
 
-//       let weatherDiv = document.getElementById('weather');
-//       weatherDiv.innerHTML = '';
+  const humidity = document.createElement("p");
+  humidity.textContent = `Humidity: ${day.main.humidity}%`;
+  dayDiv.appendChild(humidity);
 
-//       for (let i = 0; i < data.list.length; i += 8) {
-//         let date = new Date(data.list[i].dt_txt);
-//         let temp = data.list[i].main.temp;
-//         let humidity = data.list[i].main.humidity;
-//         let wind = data.list[i].wind.speed;
-
-//         let dayDiv = document.createElement('div');
-//         dayDiv.innerHTML = `
-//           <h2>${date.toLocaleDateString()}</h2>
-//           <p>Temperature: ${temp} &deg;C</p>
-//           <p>Humidity: ${humidity}%</p>
-//           <p>Wind Speed: ${wind} m/s</p>
-//         `;
-//         weatherDiv.appendChild(dayDiv);
-//       }
-//     })
-//     .catch(error => {
-//       console.log(error);
-//     });
-// }
+  return dayDiv;
+}
